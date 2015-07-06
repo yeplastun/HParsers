e677bdd9cc78898138593ee30199c1ff80681b78
Egor <yeplastun@gmail.com>
2015-07-06 12:51:28 +0300

    Added Moscow Fm

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import re
 
-pattern = re.compile('http://www.shazam.com/track/[0-9]+/'+'lose-yourself',re.IGNORECASE)
-s = 'http://www.shazam.com/track/11177141/lose-yourself'
-res = pattern.match(s)
-print(res)
+regex = '[а-я]+'
+s = 'фывsdфывфыв'
+pattern = re.compile(regex)
+for p in pattern.search(s).span():
+	print(p)
