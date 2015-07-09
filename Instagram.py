ce938eb76772cc55b9f78d0b8f7828b4b1675320
Egor <yeplastun@gmail.com>
2015-07-09 19:19:24 +0300

    Added inst

---
+++ Instagram.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from requests import get
+
+access_token = '19212580.1677ed0.6f908ddb35564fac80cd1927abc7084e'
+
+
+def getInstFollowers(artist):
+    url = 'https://api.instagram.com/v1/users/'
+    params = {'access_token': access_token, 'q': artist, 'count': 5}
+    req = get(url + 'search', params)
+    req = req.json()['data']
+    res = []
+    for user in req:
+        r = get(url + user['id'], {'access_token': access_token})
+        r = r.json()
+        res.append(r['data']['counts']['followed_by'])
+    if len(res) is 0:
+        return 'No matching users'
+    else:
+        return max(res)
